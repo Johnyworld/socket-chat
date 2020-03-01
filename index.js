8c43a937b5413067659858481a09a33ae473d249
Johny Kim <johnyworld@naver.com>
2020-03-01 19:33:38 +0900

    Add enter/out message

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-})
+});
+
+io.on('connection', (socket) => {
+    console.log('손님 입장');
+    socket.on('disconnect', () => {
+        console.log('들어올 땐 맘대로지만 나갈 땐 아니란다.')
+    });
+});
 
 http.listen(4000, () => { console.log(`listen *:4000`) });
