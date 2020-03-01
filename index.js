9031a7cbb2311581e3e8363775da8427eed6b6e9
Johny Kim <johnyworld@naver.com>
2020-03-01 20:16:43 +0900

    Add printing messages each client window in this server

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('손님 입장');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        // socket.broadcast.emit('chat message', msg);
+    });
+
     socket.on('disconnect', () => {
         console.log('들어올 땐 맘대로지만 나갈 땐 아니란다.')
     });
+
 });
 
 http.listen(4000, () => { console.log(`listen *:4000`) });
